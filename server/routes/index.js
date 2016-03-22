07a5ebceb6b8197338a675b3d1dfcdab56e680d0
Shang-Hung Tsai <shtsai7@gmail.com>
2016-03-22 17:02:29 -0400

    added geolocation API

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,7 +4,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CS411A2', status: 'ok' });
+//  res.render('index', { title: 'CS411A2', status: 'ok' });
 });
 
+
+
 module.exports = router;
+
